ffbeff6518f6704b22eaca05e0f62a83607c9c1a
jiaming <admin@dwrm.com>
2019-05-23 17:27:29 +0100

    store credentials into separate file

--- lottery/sqlalchemy_base.py
+++ lottery/sqlalchemy_base.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import json
 
-engine = create_engine('mysql+pymysql://eason:ouN]jol3@34.252.90.15:3306/lottery', echo=False)
+with open('./lottery/data/config.json') as configfile:
+    data = json.load(configfile)
+    connection_string = 'mysql+pymysql://' + data['username'] + ':' + data['password'] + '@' + data['host'] + ':' + data['port'] + '/lottery'
+
+engine = create_engine(connection_string, echo=False)
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
